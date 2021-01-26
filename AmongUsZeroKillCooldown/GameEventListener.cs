47f940fcac00eb20092852891ddcc7afe9649b5a
Logan <logon.k13@gmail.com>
2021-01-27 01:11:36 +1300

    Add project files.

---
+++ AmongUsZeroKillCooldown/GameEventListener.cs
@@ -0,0 +1,36 @@
+﻿using Impostor.Api.Events;
+using Impostor.Api.Events.Player;
+using Microsoft.Extensions.Logging;
+
+namespace Impostor.Plugins.Example.Handlers
+{
+    /// <summary>
+    ///     A class that listens for two events.
+    ///     It may be more but this is just an example.
+    ///
+    ///     Make sure your class implements <see cref="IEventListener"/>.
+    /// </summary>
+    public class GameEventListener : IEventListener
+    {
+        private readonly ILogger<ZeroKillCooldownPlugin> _logger;
+
+        public GameEventListener(ILogger<ZeroKillCooldownPlugin> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        ///     Called when the game starts
+        /// </summary>
+        /// <param name="e">
+        ///     The event you want to listen for.
+        /// </param>
+        [EventListener]
+        public void OnGameStarted(IGameStartedEvent e)
+        {
+            //Instant kill cooldown in among us
+            e.Game.Options.KillCooldown = 0.01f;
+            e.Game.SyncSettingsAsync();
+        }
+    }
+}
---
+++ AmongUsZeroKillCooldown/ZeroKillCooldownPlugin.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading.Tasks;
+using Impostor.Api.Events.Managers;
+using Impostor.Api.Plugins;
+using Impostor.Plugins.Example.Handlers;
+using Microsoft.Extensions.Logging;
+
+namespace Impostor.Plugins.Example
+{
+    /// <summary>
+    ///     The metadata information of your plugin, this is required.
+    /// </summary>
+    [ImpostorPlugin(
+        package: "antimeta.impostor.zerocooldown",
+        name: "Zero Kill Cooldown",
+        author: "LogonDev",
+        version: "1.0.0")]
+    public class ZeroKillCooldownPlugin : PluginBase // This is also required ": PluginBase".
+    {
+        /// <summary>
+        ///     A logger that works seamlessly with the server.
+        /// </summary>
+        private readonly ILogger<ZeroKillCooldownPlugin> _logger;
+        private readonly IEventManager _eventManager;
+        private IDisposable _unregister;
+
+        /// <summary>
+        ///     The constructor of the plugin. There are a few parameters you can add here and they
+        ///     will be injected automatically by the server, two examples are used here.
+        ///
+        ///     They are not necessary but very recommended.
+        /// </summary>
+        /// <param name="logger">
+        ///     A logger to write messages in the console.
+        /// </param>
+        /// <param name="eventManager">
+        ///     An event manager to register event listeners.
+        ///     Useful if you want your plugin to interact with the game.
+        /// </param>
+        public ZeroKillCooldownPlugin(ILogger<ZeroKillCooldownPlugin> logger, IEventManager eventManager)
+        {
+            _logger = logger;
+            _eventManager = eventManager;
+        }
+
+        /// <summary>
+        ///     This is called when your plugin is enabled by the server.
+        /// </summary>
+        /// <returns></returns>
+        public override ValueTask EnableAsync()
+        {
+            _unregister = _eventManager.RegisterListener(new GameEventListener(_logger));
+            return default;
+        }
+
+        /// <summary>
+        ///     This is called when your plugin is disabled by the server.
+        ///     Most likely because it is shutting down, this is the place to clean up any managed resources.
+        /// </summary>
+        /// <returns></returns>
+        public override ValueTask DisableAsync()
+        {
+            _unregister.Dispose();
+            return default;
+        }
+    }
+}
